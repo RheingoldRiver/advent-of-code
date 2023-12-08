d44daa1ec0b97b8dd4d917c4e2be1ec7cf5a4d03
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2023-12-08 00:48:18 -0600

    Day 8

--- 08/part-1.py
+++ 08/part-1.py
@@ -4,17 +4,34 @@ from copy import copy, deepcopy
 
 
 class Solver:
+    start = 'AAA'
+    end = 'ZZZ'
 
     def __init__(self):
-        with open('input.txt', 'r', encoding='utf-8') as f:
-            self.lines = [line.strip() for line in f.readlines()]
-        with open('info2.json', 'r', encoding='utf-8') as f:
+        with open('info.json', 'r', encoding='utf-8') as f:
             self.data = json.load(f)
+        self.directions = self.data["line1"] * 20000
+        self.resulting_options = [self.parse_line(line) for line in self.data["lookup"].split('\n')]
+        self.lookup = {item['start']: item for item in self.resulting_options}
+
+    @staticmethod
+    def parse_line(line):
+        start = line.split(' = ')[0]
+        results = line.split(' = ')[1].replace('(', '').replace(')', '')
+        return {
+            'start': start,
+            'L': results.split(', ')[0],
+            'R': results.split(', ')[1],
+        }
 
     def run(self):
         total = 0
-        product = 1
-
+        cur = self.start
+        for char in self.directions:
+            total += 1
+            cur = self.lookup[cur][char]
+            if cur == self.end:
+                break
 
         return total
 
---
+++ 08/part-2.py
@@ -0,0 +1,48 @@
+import json
+import math
+import re
+import numpy as np
+
+
+class Solver:
+    start = 'AAA'
+    end = 'ZZZ'
+
+    def __init__(self):
+        with open('info.json', 'r', encoding='utf-8') as f:
+            self.data = json.load(f)
+        self.directions = self.data["line1"] * 2000000
+        self.resulting_options = [self.parse_line(line) for line in self.data["lookup"].split('\n')]
+        self.lookup = {item['start']: item for item in self.resulting_options}
+
+    @staticmethod
+    def parse_line(line):
+        start = line.split(' = ')[0]
+        results = line.split(' = ')[1].replace('(', '').replace(')', '')
+        return {
+            'start': start,
+            'L': results.split(', ')[0],
+            'R': results.split(', ')[1],
+        }
+
+    def run(self):
+        total = 0
+        cur = [loc for loc in self.lookup.keys() if loc.endswith('A')]
+        prime_factors = [None] * len(cur)
+        for char in self.directions:
+            total += 1
+            for i, loc in enumerate(cur):
+                new_square = self.lookup[loc][char]
+                cur[i] = new_square
+                if new_square.endswith('Z') and prime_factors[i] is None:
+                    prime_factors[i] = total
+            print(prime_factors)
+            if all(x is not None for x in prime_factors):
+                break
+        print(prime_factors)
+
+        return np.lcm.reduce(prime_factors)
+
+
+if __name__ == '__main__':
+    print(Solver().run())
